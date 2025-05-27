9c56fc4eb874845569a2a7138f5d6473bdaf6bc0
Logan Valeski <loganvaleski@gmail.com>
2025-05-27 15:56:05 -0600

    Added proper mocking for tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native-firebase)/)'
+  ],
+  setupFiles: ['./jest/setup.js'],
 };
