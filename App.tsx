6a686421370875ce37af2c4257aad1af0f0a0651
Logan Valeski <loganvaleski@gmail.com>
2025-06-05 22:18:49 -0400

    RevenueCat initial implementation

--- App.tsx
+++ App.tsx
@@ -11,9 +11,12 @@ import { AuthProvider } from './src/contexts/AuthContext';
 import { AuthStack } from './src/navigation/AuthStack';
 import { ThemeProvider } from './src/design-system/ThemeProvider';
 import TrackPlayer from 'react-native-track-player';
+import Purchases from 'react-native-purchases';
 
 const App = () => {
   useEffect(() => {
+    Purchases.setLogLevel(Purchases.LOG_LEVEL.DEBUG);
+    Purchases.configure({ apiKey: 'goog_jmulZDEicQKHZvTRTaZtfWYydrO' });
     (async () => {
       await TrackPlayer.setupPlayer();
     })();
