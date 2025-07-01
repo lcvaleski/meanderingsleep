6104ad5e67f8c57cb30970e87f07900cd646d59f
Logan Valeski <loganvaleski@gmail.com>
2025-07-01 16:38:35 -0600

    Removing erreneous files

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-// import App from './SimpleApp';
 import {name as appName} from './app.json';
 import TrackPlayer from 'react-native-track-player';
 
