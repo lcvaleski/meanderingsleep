e98159392aee10f289d94623d78056234748ce43
Logan Valeski <loganvaleski@gmail.com>
2025-07-13 14:04:43 -0700

    Removing Crashlytics explicit upload for android as it happens in AAB upload

--- App.tsx
+++ App.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { AuthProvider } from './src/contexts/AuthContext';
 import { RootNavigator } from './src/navigation/RootNavigator';
 import initializeFirebase from './src/config/firebase';
+import crashlytics from '@react-native-firebase/crashlytics';
 
 // Initialize Firebase on app start
 initializeFirebase();
+
+// Initialize Crashlytics
+crashlytics().setCrashlyticsCollectionEnabled(true);
 
 function App(): React.JSX.Element {
   return (
