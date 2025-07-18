6dcdfae714bf25e6275fd7d0e0cd8b6505944987
Logan Valeski <loganvaleski@gmail.com>
2025-07-18 17:08:13 -0600

    Disabling text scalling accessibility

--- index.js
+++ index.js
@@ -2,10 +2,17 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Text, TextInput} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import TrackPlayer from 'react-native-track-player';
+
+// Disable font scaling globally
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.allowFontScaling = false;
 
 AppRegistry.registerComponent(appName, () => App);
 TrackPlayer.registerPlaybackService(() => require('./service'));
